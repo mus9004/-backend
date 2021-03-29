5ef9668557edbb636c80f5fcef2de034d0b3e49d
mus9004 <mus9004@yahoo.es>
2021-03-29 15:04:40 -0600

    Add files via upload

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -277,7 +277,7 @@ router.post('/user/loginUserAd', (req, res) =>{
             const resultado= bcrypt.compareSync(contrasena, rows[0].password)
             console.log(resultado+"ddddddddddddddddddddddddddddddddd");
             if (resultado) {
-                    res.send(JSON.parse('{"resul": "true" }'));
+                    res.send(JSON.parse('{"resul": "true","error":"0" }'));
                     console.log(resultado);
             } else{
                 res.send(JSON.parse('{"resul": 401.2,"error":"Error de contraseña"}'));
